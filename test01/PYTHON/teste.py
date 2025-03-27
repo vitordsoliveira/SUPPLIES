1559953cb99eea030f05a05c2e88b80c7a0682fc
vitordsoliveira <vitordaniel.silva32@gmail.com>
2025-03-27 19:57:54 -0300

    details

--- test01/PYTHON/teste.py
+++
@@ -1,8 +0,0 @@
-nome = input("Qual seu nome? ")
-idade = int(input("Qual sua idade? "))
-if idade >= 18:
-        print(nome, "é maior de idade.")
-elif idade >= 14 or idade < 18:
-    print(nome, "adolescente.")
-else:
-    print("Criança.")
