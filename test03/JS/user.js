d87dc3a55f886fcc630ebc5dc29de98139093d51
vitordsoliveira <vitordaniel.silva32@gmail.com>
2025-04-01 20:42:52 -0300

    register funcionando

--- test03/JS/user.js
+++ test03/JS/user.js
@@ -1,44 +1,40 @@
 async function cadUser() {
+
     let name = document.getElementById('IDname').value;
 
     let email = document.getElementById('IDemail').value;
-
-    let userType = 1;
 
     let password = document.getElementById('IDpassword').value;
 
     let cpf_cnpj = document.getElementById('IDcpf_cnpj').value;
 
-    let termos = 1;
-
     let birthday = document.getElementById('IDbirthday').value;
+
+    let url = "https://go-wash-api.onrender.com/api/user";
 
     let dados = {        
         "name": name,
         "email": email,
-        "user_type_id": userType,
+        "user_type_id": 1,
         "password": password,
         "cpf_cnpj": cpf_cnpj,
-        "terms": termos,
+        "terms": 1,
         "birthday": birthday
     };
 
-    url = "https://go-wash-api.onrender.com/api/user"
-
-    console.log("Dados enviados:", dados); // Adicione esta linha para verificar os dados
-
-    let api = await fetch(url ,{
-            method: "POST",
-            body: JSON.stringify(dados),
-            headers:{'Content-Type': 'application/json'}
-        }
-    );
+    let api = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(dados),
+        headers: { 'Content-Type': 'application/json' }
+    });
     
     if (api.ok) {
         let response = await api.json();
         console.log(response);
-        return;
+        alert("CADASTRO REALIZADO COM SUCESSO!")
+
+    } else {
+        let response = await api.json();
+        console.log(response);
     }
-    let responseError = await api.json();
-    console.log(responseError);
 }
