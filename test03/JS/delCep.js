3ee0cc0680cbfec1d90cb967d821bb6542fa38ec
vitordsoliveira <vitordaniel.silva32@gmail.com>
2025-05-20 20:19:34 -0300

    Feito

--- test03/JS/delCep.js
+++ test03/JS/delCep.js
@@ -12,33 +12,18 @@ async function delCep(id) {
 
         if (response.ok) {
             console.log("Endereço deletado com sucesso.");
-            showMessage("Endereço deletado com sucesso!", "success");
+            alert("Endereço deletado com sucesso!", "success");
             location.reload();
         } else {
             console.log("Erro ao deletar endereço.");
-            showMessage("Erro ao deletar endereço.", "error");
+            alert("Erro ao deletar endereço.", "error");
         }
     } catch (error) {
         console.error("Erro na requisição:", error);
-        showMessage("Erro na comunicação com o servidor.", "error");
+        alert("Erro na comunicação com o servidor.", "error");
     }
 }
 
-function showMessage(mensagem, tipo = "info") {
-    let msgBox = document.getElementById("mensagem");
 
-    if (!msgBox) {
-        msgBox = document.createElement("div");
-        msgBox.id = "mensagem";
-        msgBox.style.marginTop = "10px";
-        document.body.prepend(msgBox);
-    }
-
-    msgBox.innerText = mensagem;
-    msgBox.style.display = "block";
-    msgBox.style.padding = "10px";
-    msgBox.style.borderRadius = "5px";
-    msgBox.style.color = tipo === "success" ? "green" : "red";
-}
 
 
