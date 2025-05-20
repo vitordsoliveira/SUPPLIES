cb1fc4aedb4f8e80f124ee533166e44e1b07b36a
vitordsoliveira <vitordaniel.silva32@gmail.com>
2025-05-20 20:14:05 -0300

    sa

--- test03/JS/listagem.js
+++ test03/JS/listagem.js
@@ -25,16 +25,10 @@ async function listagem() {
         <td>${item.number}</td>
         <td>${item.cep}</td>
         <td>${item.complement}</td>
-<<<<<<< HEAD
         <td><input class="" type="button" value="Atualizar" onclick="attCep()"></input></td>
         <td><input class="" type="button" value="Deletar" onclick="delCep('${item.id || item._id}')" /></td>`;
 
             tabela.appendChild(linha);
-=======
-        <td><input class="btnAttCep btnInputCep" value="Atualizar"></input></td>
-        <td><input class="btnDelCep btnInputCep" value="Deletar"></input></td> `;
-        tabela.appendChild(linha);
->>>>>>> 302fd08341e8d7824b32958373ec297548dfa1a5
         });
 
     } else {
