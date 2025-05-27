888edd179b23542411330010ab373a2f9753c525
vitordsoliveira <vitordaniel.silva32@gmail.com>
2025-05-27 19:23:51 -0300

    arrumando o footer, ajustando H2 do footer

--- test03/JS/listagem.js
+++ test03/JS/listagem.js
@@ -26,7 +26,7 @@ async function listagem() {
         <td>${item.cep}</td>
         <td>${item.complement}</td>
         <td class="btnCenter"><input class="btnInputCep btnAtt" type="button" value="ATUALIZAR" onclick="attCep()"></input></td>
-        <td class="btnCenter"><input class="btnInputCep btnDel" type="button" value="DELETAR" onclick="delCep('${item.id || item._id}')" /></td>`;
+        <td class="btnCenter"><input class="btnInputCep btnDel" type="button" value="DELETAR" onclick="delCep('${item.id || item._id}')"/></td>`;
         tabela.appendChild(linha);
         });
 
