1487ab2290c5875f7dbfb2efc6be31ba07c4fb5b
vitordsoliveira <vitordaniel.silva32@gmail.com>
2025-06-09 10:37:38 -0300

    regularizando

---
+++ test01/JS/logUser.js
@@ -0,0 +1,32 @@
+async function logUser() {
+    let email = document.getElementById('IDemailLog').value;
+    let password = document.getElementById('IDpasswordLog').value;
+    let location = 'https://go-wash-api.onrender.com/api/login';
+
+    let data = {
+        "email": email,
+        "password": password,
+        "user_type_id": 1
+    };
+
+    let api = await fetch(location, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (api.ok) {
+        let response = await api.json();
+        console.log(response);
+        alert("LOGIN REALIZADO COM SUCESSO!");
+        localStorage.setItem('email', JSON.stringify(email));
+        localStorage.setItem('password', JSON.stringify(password));
+        localStorage.setItem('token', response.access_token);
+        window.location = "listagem.html";
+        return;
+    } else {
+        let response = await api.json();
+        console.log(response);
+        alert("LOGIN NÃO REALIZADO!");
+    }
+}
