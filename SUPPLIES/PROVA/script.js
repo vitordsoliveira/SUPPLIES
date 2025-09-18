672d286b899c972b5c64da0aff8a19d9646cdabe
vitordsoliveira <vitordaniel.silva32@gmail.com>
2025-09-18 10:35:37 -0300

    tirando as pastas testes

--- SUPPLIES/PROVA/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("form");
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        alert("Mensagem enviada com sucesso!");
-        form.reset();
-    });
-});
